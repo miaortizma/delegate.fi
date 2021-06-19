cb61cb8f426112415d747a48ec2e27b81ed3bd71
Kankan-0 <kankandev@protonmail.com>
2021-06-19 22:01:05 +0530

    Updating the delegated info when user delegates.

--- packages/hardhat/contracts/DelegateCreditManager.sol
+++ packages/hardhat/contracts/DelegateCreditManager.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.6.0 <0.9.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 import "./interface/ILendingPool.sol";
 import "./interface/IDebtToken.sol";
@@ -11,9 +12,17 @@ import "./interface/IProtocolDataProvider.sol";
 
 contract DelegateCreditManager {
   using SafeERC20 for IERC20;
+  using SafeMath for uint256;
 
   ILendingPool lendingPool;
   IProtocolDataProvider provider;
+
+  struct DelegatorInfo {
+    uint256 amountDelegated;
+    uint256 earnings;
+  }
+  mapping(address => uint256) public totalDelegatedAmounts;
+  mapping(address => DelegatorInfo) public delegators;
 
   constructor(ILendingPool _lendingPool) {
     lendingPool = _lendingPool;
@@ -29,5 +38,8 @@ contract DelegateCreditManager {
     (, , address variableDebtTokenAddress) = provider.getReserveTokensAddresses(_asset);
     
     IDebtToken(variableDebtTokenAddress).approveDelegation(address(this), _amount);
+    // update the total delgated amount, also update the delegators info.
+    totalDelegatedAmounts[_asset].add(_amount);
+    delegators[msg.sender].amountDelegated.add(_amount);
   }
 }
