4aadfcf36a99b21f94ea87ed012aadf00777d156
petrovska-petro <84875062+petrovska-petro@users.noreply.github.com>
2021-06-19 15:36:07 +0100

    Include mapping in DelegateCreditManager.sol for accounting purposes and f/e dashboard show off

--- packages/hardhat/contracts/DelegateCreditManager.sol
+++ packages/hardhat/contracts/DelegateCreditManager.sol
@@ -10,10 +10,19 @@ import "./interface/IDebtToken.sol";
 import "./interface/IProtocolDataProvider.sol";
 
 contract DelegateCreditManager {
+  using SafeMath for uint256;
   using SafeERC20 for IERC20;
+  
+  struct DelegatorInfo {
+    uint256 amountDelegated;
+    uint256 earnings;
+  }
 
   ILendingPool lendingPool;
   IProtocolDataProvider provider;
+  
+  // Tracks delegators info, useful more dashboards in f/e and inner accounting
+  mapping (address => DelegatorInfo) public delegators;
 
   constructor(ILendingPool _lendingPool) {
     lendingPool = _lendingPool;
@@ -24,10 +33,16 @@ contract DelegateCreditManager {
    * @dev Allows user to delegate to our protocol (first point of contact user:protocol)
    * @param _asset The asset which is delegated
    * @param _amount The amount delegated to us to manage
+   * @notice we do not emit event as  `approveDelegation` emits -> BorrowAllowanceDelegated
   **/
   function delegateCreditLine(address _asset, uint256 _amount) {
     (, , address variableDebtTokenAddress) = provider.getReserveTokensAddresses(_asset);
     
     IDebtToken(variableDebtTokenAddress).approveDelegation(address(this), _amount);
+
+    DelegatorInfo storage delegator = delegators[msg.sender];
+    
+    // no need for sub || add operation, as approveDelegation auto-updates either increasing or decreasing allowance
+    delegator.amountDelegated = _amount;
   }
 }
