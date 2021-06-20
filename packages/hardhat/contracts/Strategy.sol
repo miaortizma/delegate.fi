b5a58c146e3821fbeae4ce46e6862bb724a6e87d
petrovska-petro <84875062+petrovska-petro@users.noreply.github.com>
2021-06-20 22:12:36 +0100

    Add Aave-Curve gauge interface and skeleton for harvest method in Strategy.sol

--- packages/hardhat/contracts/Strategy.sol
+++ packages/hardhat/contracts/Strategy.sol
@@ -7,19 +7,26 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 
 import "./interface/ICurvePool.sol";
+import "./interface/IAaveGauge.sol";
 
 contract Strategy is Ownable {
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
-
+  
+  /// Curve.fi related contracts
   ICurvePool public curvePool = ICurvePool(address(0x445FE580eF8d70FF569aB36e80c647af338db351));
+  IAaveGauge public aaveGauge = IAaveGauge(address(0xe381C25de995d62b453aF8B931aAc84fcCaa7A62));
   IERC20 public lpCRV = IERC20(address(0xE7a24EF0C5e95Ffb0f6684b813A78F2a3AD7D171));
 
   int128 public curveId;
 
   address public delegateFund;
   address public want; // it could DAI, USDC or USDT (which?)
-
+  
+  /// Tokens involved in the strategy
+  address constant public CRV = address(0x172370d5Cd63279eFa6d502DAB29171933a610AF);
+  address constant public WMATIC = address(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270);
+  
   uint256 public constant MAX_FEE = 10000;
   uint256 public REVENUE_FEE = 1000;
   uint256 public depositLimit;
@@ -27,6 +34,13 @@ contract Strategy is Ownable {
   event UpdateDepositLimit(uint256 depositLimit, uint256 timestamp);
   event Deposited(uint256 amountDeposited, uint256 timestamp);
   event RepayDebt(uint256 repaidDebt, uint256 existingAaveDebt);
+  event Harvest(
+      uint256 curveHarvested, 
+      uint256 wmaticHarvested,
+      uint256 curveProtocolFee,
+      uint256 wmaticProtocolFee,
+      uint256 indexed blockNumber
+  );
 
   constructor(address[2] memory _initialConfig, uint256 _limit, int128 _curveId) public {
     delegateFund = _initialConfig[0];
@@ -38,24 +52,32 @@ contract Strategy is Ownable {
   }
   
   /// --- View Functions ---
-
+  
+  /// @notice Amount of `want` via lp curve relationship
   function lpCurveToWant() public view returns (uint256) {
     uint256 lpRatio = curvePool.get_virtual_price().div(10**18);
 
-    uint256 wantFromLp = lpRatio.mul(lpCRV.balanceOf(address(this))).div(10**18);
+    uint256 wantFromLp = lpRatio.mul(balanceInGauge()).div(10**18);
 
     return wantFromLp;
   }
+  
+  /// @notice Amount of lp tokens deposited in Gauge
+  function balanceInGauge() public view returns (uint256) {
+    return aaveGauge.balanceOf(address(this));
+  }
 
-  /// @notice Idle want in strat
+  /// @notice Idle want in strategy
   function balanceOfWant() public view returns (uint256) {
     return IERC20(want).balanceOf(address(this));
   }
-
-  function totalAssets() internal view returns (uint256) {
-    return balanceOfWant().add(0);
+  
+  /// @notice Provides insight of how many assets are under management expressed in `want`
+  function totalAssets() public view returns (uint256) {
+    return balanceOfWant().add(lpCurveToWant());
   }
-
+  
+  /// @dev Harvest accum rewards from Gauge (CRV & WMATIC)
   function deposit(uint256 _amount) external onlyOwner {
     require(_amount > 0, "nothing!");
 
@@ -74,6 +96,37 @@ contract Strategy is Ownable {
     IERC20(want).safeTransferFrom(msg.sender, address(this), amount);
 
     emit Deposited(amount, block.timestamp);
+  }
+  
+  /// @dev Harvest accum rewards from Gauge (CRV & WMATIC)
+  function harvest() external onlyOwner {
+    aaveGauge.claim_rewards(address(this));
+
+    uint256 curveBal = IERC20(CRV).balanceOf(address(this));
+    uint256 wmaticBal = IERC20(WMATIC).balanceOf(address(this));
+
+    (uint256 curveFee, uint256 wmaticFee) = protocolFee(curveBal, wmaticBal);
+
+    curveBal = curveBal.sub(curveFee);
+    wmaticBal = wmaticBal.sub(wmaticFee);
+    
+    if (wmaticBal > 0 || curveBal > 0) {
+        // 1. It would be sold in the secondary market for more `want`
+    }
+
+    emit Harvest(curveBal, wmaticBal, curveFee, wmaticFee, block.number);
+  }
+
+  function protocolFee(uint256 curveHarvested, uint256 wmaticHarvested) internal returns (uint256 curveFee, uint256 wmaticFee) {
+    curveFee = curveHarvested.mul(REVENUE_FEE).div(MAX_FEE);
+    
+    IERC20(CRV).safeTransfer(delegateFund, curveFee);
+
+    wmaticFee = wmaticHarvested.mul(REVENUE_FEE).div(MAX_FEE);
+
+    IERC20(WMATIC).safeTransfer(delegateFund, wmaticFee);
+
+    return (curveFee, wmaticFee);
   }
   
   /** 
---
+++ packages/hardhat/contracts/interface/IAaveGauge.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+interface IAaveGauge {
+    function claim_rewards(address _addr) external;
+
+    function deposit(uint256 _value) external;
+
+    function withdraw(uint256 _value) external;
+
+    function balanceOf(address _addr) external view returns (uint256);
+}
